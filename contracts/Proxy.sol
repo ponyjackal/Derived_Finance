628914d29e66717aa18ad26da046dd3fe6fde3b7
ponyjackal <pony1125@outlook.com>
2021-12-29 05:03:56 -0800

    fix errors on delegatecall

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -103,15 +103,15 @@ contract Proxy is Owned {
             assembly {
                 /* Copy call data into free memory region. */
                 let free_ptr := mload(0x40)
-                calldatacopy(free_ptr, 0, calldatasize)
+                calldatacopy(free_ptr, 0, calldatasize())
 
                 /* Forward all gas and call data to the target contract. */
-                let result := delegatecall(gas, sload(target_slot), free_ptr, calldatasize, 0, 0)
-                returndatacopy(free_ptr, 0, returndatasize)
+                let result := delegatecall(gas(), sload(target_slot), free_ptr, calldatasize(), 0, 0)
+                returndatacopy(free_ptr, 0, returndatasize())
 
                 /* Revert if the call failed, otherwise return the result. */
-                if iszero(result) { revert(free_ptr, returndatasize) }
-                return(free_ptr, returndatasize)
+                if iszero(result) { revert(free_ptr, returndatasize()) }
+                return(free_ptr, returndatasize())
             }
         } else {
             /* Here we are as above, but must send the messageSender explicitly
@@ -119,14 +119,14 @@ contract Proxy is Owned {
             target.setMessageSender(msg.sender);
             assembly {
                 let free_ptr := mload(0x40)
-                calldatacopy(free_ptr, 0, calldatasize)
+                calldatacopy(free_ptr, 0, calldatasize())
 
                 /* We must explicitly forward ether to the underlying contract as well. */
-                let result := call(gas, sload(target_slot), callvalue, free_ptr, calldatasize, 0, 0)
-                returndatacopy(free_ptr, 0, returndatasize)
+                let result := call(gas(), sload(target_slot), callvalue(), free_ptr, calldatasize(), 0, 0)
+                returndatacopy(free_ptr, 0, returndatasize())
 
-                if iszero(result) { revert(free_ptr, returndatasize) }
-                return(free_ptr, returndatasize)
+                if iszero(result) { revert(free_ptr, returndatasize()) }
+                return(free_ptr, returndatasize())
             }
         }
     }
