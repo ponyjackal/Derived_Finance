90118284934d7f517eaedd4742ce70e615f79b7d
ponyjackal <pony1125@outlook.com>
2022-01-04 04:34:58 -0800

    fix compile-time constant in SynthetixState

--- contracts/SynthetixState.sol
+++ contracts/SynthetixState.sol
@@ -79,7 +79,7 @@ contract SynthetixState is State, LimitedSetup {
     // may not be issued against a given value of SNX.
     uint public issuanceRatio = SafeDecimalMath.unit() / 5;
     // No more synths may be issued than the value of SNX backing them.
-    uint constant MAX_ISSUANCE_RATIO = SafeDecimalMath.unit();
+    uint public MAX_ISSUANCE_RATIO = SafeDecimalMath.unit();
 
     // Users can specify their preferred currency, in which case all synths they receive
     // will automatically exchange to that preferred currency upon receipt in their wallet
