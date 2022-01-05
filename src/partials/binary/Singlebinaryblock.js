ccf2a3ff386fd36477698a59892b1a679162ae23
bbshark99 <andy.solution99@gmail.com>
2022-01-05 09:53:16 -0500

    feat: added questions to binary page

--- src/partials/binary/Singlebinaryblock.js
+++ src/partials/binary/Singlebinaryblock.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StarBorderIcon from "@mui/icons-material/StarBorder";
 import Checkbox from "@mui/material/Checkbox";
 import { Link } from "react-router-dom";
 
 import Progressbar from "./Progressbar";
 import image from "../../images/user-36-05.jpg";
-import { toShortAddress } from "../../utils/Utils";
+import { toShortAddress, toFriendlyTimeFormat, toTimer } from "../../utils/Utils";
 
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
-const Singlebinaryblock = ({ title, questionId }) => {
+const Singlebinaryblock = ({ title, questionId, resolveTime }) => {
+  const [time, setTime] = useState("00:00:00:00");
+
+  useEffect(() => {
+    if (!resolveTime) return;
+
+    const t = (+resolveTime) * 1000;
+    const c = (new Date()).getTime();
+    if (t < c) return;
+
+    const timer = setInterval(() => {
+      const t = (+resolveTime) * 1000;
+      const c = (new Date()).getTime();
+
+      if (t > c) {
+        setTime(toTimer(t - c))
+      } else {
+        setTime("00:00:00:00");
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [resolveTime]);
+
   return (
     <div className="p-2 bg-secondary rounded-lg">
       <div className="flex items-center justify-between">
@@ -34,11 +59,12 @@ const Singlebinaryblock = ({ title, questionId }) => {
           </div>
           <div className="flex items-center justify-between px-5 py-2">
             <p className="text-gray-400">Time Remaining</p>
-            <p className="text-white">20:01:01:01</p>
+            <p className="text-white">{time}</p>
           </div>
           <div className="flex items-center justify-between px-5 py-2">
             <p className="text-gray-400">Expiry Date</p>
-            <p className="text-white">Nov 30, 21 | 00:30</p>
+            {/* <p className="text-white">Nov 30, 21 | 00:30</p> */}
+            <p className="text-white">{toFriendlyTimeFormat(+resolveTime || 0)}</p>
           </div>
           <div className="flex items-center justify-between px-5 py-2">
             <p className="text-gray-400">Strike Price</p>
