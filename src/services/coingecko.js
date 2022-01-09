a8dbcf9f0810111c00241ceb374c9ae10eef1bff
bbshark99 <andy.solution99@gmail.com>
2022-01-09 10:48:24 -0600

    fix: coingecko api base url

--- src/services/coingecko.js
+++ src/services/coingecko.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-  baseURL: 'https://api.coingecko.com/api/v3/simple',
+  baseURL: 'https://api.coingecko.com/api/v3',
 });
 
 export const getPrice = async (id) => {
   try {
-    const resp = await httpClient.get(`/price?ids=${id}&vs_currencies=usd`);
+    const resp = await httpClient.get(`/simple/price?ids=${id}&vs_currencies=usd`);
     return resp.data[id].usd;
   } catch (error) {
     console.error('Getting price error: ', error.message);
