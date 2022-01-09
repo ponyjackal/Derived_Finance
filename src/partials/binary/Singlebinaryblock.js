6a9b83d9004b9adbcc7fb9306361b3c4c133ba94
bbshark99 <andy.solution99@gmail.com>
2022-01-09 09:20:56 -0600

    feat: added prices to card

--- src/partials/binary/Singlebinaryblock.js
+++ src/partials/binary/Singlebinaryblock.js
@@ -1,32 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { useWeb3React } from "@web3-react/core";
-// import StarBorderIcon from "@mui/icons-material/StarBorder";
-// import Checkbox from "@mui/material/Checkbox";
-import Skeleton from '@mui/material/Skeleton';
 import { Link } from "react-router-dom";
 import { BigNumber } from 'bignumber.js';
 
-import { useMarket } from "../../context/market";
 import Progressbar from "./Progressbar";
 // import image from "../../images/user-36-05.jpg";
 import { toShortAddress, toFriendlyTimeFormat, toTimer } from "../../utils/Utils";
-import { toShortAmount } from "../../utils/Contract";
+import { toShort18 } from "../../utils/Contract";
 
 // const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
-const Singlebinaryblock = ({ title, questionId, resolveTime, createTime, strikePrice, token, status }) => {
-  const { library, active, account } = useWeb3React();
-  const { MarketContract } = useMarket();
-
-  const [loading, setLoading] = useState(false);
-  const [loadingBalance, setLoadingBalance] = useState(false);
+const Singlebinaryblock = ({ title, questionId, resolveTime, createTime, long, short }) => {
   const [time, setTime] = useState("00:00:00:00");
   const [progress, setProgress] = useState(100);
-  const [endPrice, setEndPrice] = useState(0);
-  const [balances, setBalances] = useState({
-    long: 0,
-    short: 0,
-  });
 
   useEffect(() => {
     if (!resolveTime || !createTime) return;
@@ -54,43 +39,6 @@ const Singlebinaryblock = ({ title, questionId, resolveTime, createTime, strikeP
       clearInterval(timer);
     };
   }, [resolveTime, createTime]);
-
-  useEffect(() => {
-    if (!strikePrice || !token || !library || !active) return;
-
-    const initialize = async () => {
-      setLoading(true);
-
-      const amount = await toShortAmount(token, strikePrice, library);
-      setEndPrice(amount);
-
-      setLoading(false);
-    };
-
-    initialize();
-  }, [strikePrice, token, library, active]);
-
-  useEffect(() => {
-    if (!questionId || !MarketContract || !account) return;
-
-    const initialize = async () => {
-      setLoadingBalance(true);
-
-      const longId = await MarketContract.generateAnswerId(questionId, 0);
-      const shortId = await MarketContract.generateAnswerId(questionId, 0);
-
-      const long = await MarketContract.balanceOf(account, longId);
-      const short = await MarketContract.balanceOf(account, shortId);
-
-      setBalances({
-        long: new BigNumber(long.toString()).toFixed(),
-        short: new BigNumber(short.toString()).toFixed(),
-      });
-      setLoadingBalance(false);
-    };
-
-    initialize();
-  }, [questionId, account, MarketContract]);
 
   return (
     <div className="p-2 bg-secondary rounded-lg">
@@ -120,44 +68,23 @@ const Singlebinaryblock = ({ title, questionId, resolveTime, createTime, strikeP
           </div>
           <div className="flex items-center justify-between px-5 py-2">
             <p className="text-gray-400">Expiry Date</p>
-            {/* <p className="text-white">Nov 30, 21 | 00:30</p> */}
             <p className="text-white">{toFriendlyTimeFormat(+resolveTime || 0)}</p>
           </div>
           <div className="flex items-center justify-between px-5 py-2">
-            <p className="text-gray-400">Strike Price</p>
-            {loading ? <Skeleton variant="text" /> : (
-              <p className="text-white">${endPrice}</p>
-            )}
+            <p className="text-gray-400">Long</p>
+            <p className="text-white">${toShort18(new BigNumber(long || 0)).toFixed(2)}</p>
           </div>
-          {/* <div className="flex items-center justify-between px-5 py-2">
-            <p className="text-gray-400">Current Asset Price</p>
-            <p className="text-white">238$</p>
-          </div> */}
-        </div>
-        {loading ? <Skeleton variant="text" /> : (
-          <Progressbar
-            bgcolor="#86C440"
-            progress={progress}
-            height={15}
-            className="text-white"
-          />
-        )}
-        {loadingBalance ? (<Skeleton variant="text" />) : (
           <div className="flex items-center justify-between px-5 py-2">
-            <p className="text-white font-medium">{balances.long}</p>
-            <p className="text-white font-medium">{balances.short}</p>
+            <p className="text-gray-400">Short</p>
+            <p className="text-white">${toShort18(new BigNumber(short || 0)).toFixed(2)}</p>
           </div>
-        )}
-        {status === "READY" && (
-          <>
-            {loading ? <Skeleton variant="text" /> : (
-              <div className="flex items-center justify-between px-5 py-2">
-                <button className="px-6 py-2 text-sm text-white font-medium bg-headings rounded-md cursor-pointer">LONG</button>
-                <button className="px-6 py-2 text-sm text-white font-medium bg-red-600 rounded-md cursor-pointer">SHORT</button>
-              </div>
-            )}
-          </>
-        )}
+        </div>
+        <Progressbar
+          bgcolor="#86C440"
+          progress={progress}
+          height={15}
+          className="text-white"
+        />
       </Link>
     </div>
   );
