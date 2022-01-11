3d18cc9f5021c7e3add509161ccadb3bac499ea6
bbshark99 <andy.solution99@gmail.com>
2022-01-11 08:58:16 -0600

    fix: updated contract address & abi

--- src/context/address.js
+++ src/context/address.js
@@ -1,9 +1,11 @@
 export const contractAddresses = {
   market: {
+    4: "0x5D02b999d16c47D02b6A16f22E3591F92c92Db59",
     56: "",
-    97: "0x90b47e836a08F6781BB6117Fa89Fb5a471aA633D",
+    97: "0x2FD6923dD54F9AaeEd5744118D13D6291C64093f",
   },
   derivedToken: {
+    4: "0xAffAe6427D24b81B749b12cB529B1720BE287991",
     56: "",
     97: "0xff8422C90C165e5a8492E2DB06b4240D22D5AA3F",
   },
