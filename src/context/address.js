97c6e84fafdaa11e74e36988612126f4ca211b01
kecrypto <kelee.life@gmail.com>
2022-01-25 11:21:31 -0600

    fix: linter

--- src/context/address.js
+++ src/context/address.js
@@ -1,26 +1,28 @@
 export const contractAddresses = {
   market: {
-    4: "",
     56: "",
     97: "0xbd58e8dAeca967749A271b47aBf390D626B5e6cd",
   },
   derivedToken: {
-    4: "",
     56: "",
     97: "0xe92C13C39c9c2F1589C1d9dedAad06057BF3C593",
   },
-  usdx: {
-    56: "",
-    97: "0xCDE40782D7450dAb763bfa80e497351f7C0532aC",
-  },
+  // Main ERC20
   dvdx: {
     56: "",
     97: "0x3Aca0FfE99fF773d27C3F95432Bb02A1eCB1934f",
   },
+  // Asset ERC20
+  usdx: {
+    56: "",
+    97: "0xCDE40782D7450dAb763bfa80e497351f7C0532aC",
+  },
+  // Fee Pool
   pool: {
     56: "",
     97: "0x1A801a6F4a063ff5cc6A788E8416176a693ac82C",
   },
+  // Depot
   depot: {
     56: "",
     97: "0xD07E3Ea215610AFD5011688902342ca403AecAF2",
