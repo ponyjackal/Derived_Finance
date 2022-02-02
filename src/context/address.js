38e72622f098ee561a0cb3d574406d66a5bd5a5d
kecrypto <kelee.life@gmail.com>
2022-02-02 10:24:12 -0600

    feat: updated contract addresses

--- src/context/address.js
+++ src/context/address.js
@@ -10,21 +10,21 @@ export const contractAddresses = {
   // Main ERC20
   dvdx: {
     56: "",
-    97: "0x547Ec0aa6fdAf55A55939960B3BAdc6D9407b031",
+    97: "0x2448431fdC02d1f5c308C84c2020566003D2Cc9f",
   },
   // Asset ERC20
   usdx: {
     56: "",
-    97: "0x567FBbbD6C95a5072451db829d6480ffA1324757",
+    97: "0xE41B000268eDBFc239988237D7Cc6B995aD3e1Dc",
   },
   // Fee Pool
   feePool: {
     56: "",
-    97: "0x1ecbD390c948c59266CC1eEA110375804834271c",
+    97: "0x81DAca305Ef56c4b9791758C9179973238b346D0",
   },
   // Depot
   depot: {
     56: "",
-    97: "0xd3b86a852d5e99922928Ab67D1FEEDBDAD9A8607",
+    97: "0xD2b8514f4cd73557a3cDE089E76B1A2D46bdE10B",
   },
 };
