d627119bada09d59c4b5cef11cc9f927efb92687
Sree Vinayak Balasubramaniam <svinayak@8kmiles.com>
2022-02-21 11:09:19 -0800

    remove unnecessary wallets from menu

--- src/partials/header/UserMenu.js
+++ src/partials/header/UserMenu.js
@@ -5,10 +5,10 @@ import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 import Transition from "../../utils/Transition";
 import Meta from "../../images/meta-mask.png";
-import Binance from "../../images/binance.png";
 import Wallet from "../../images/wallet-connect.png";
-import Portis from "../../images/portis.png";
 import Connectedtabs from "./Connectedtabs";
+// import Binance from "../../images/binance.png";
+// import Portis from "../../images/portis.png";
 
 import { useConnector } from "../../context/connector";
 import { ConnectorNames, Connectors } from "../../utils/Connectors";
@@ -92,6 +92,11 @@ function UserMenu() {
                   <img src={Meta} className="w-10 mr-5" alt="" />
                   Metamask
                 </li>
+
+                <li className="flex text-white font-bold text-lg items-center bg-secondary m-3 p-2 rounded-lg h-14 cursor-pointer" onClick={() => connectWallet("injected")}>
+                  <img src={Wallet} className="w-10 mr-5" alt="" />
+                  Wallet Connect
+                </li>
                 {/* <li
                   className="flex text-white font-bold text-lg items-center bg-secondary m-3 p-2 rounded-lg h-14 cursor-pointer"
                   onClick={() => connectWallet("injected")}
@@ -99,10 +104,6 @@ function UserMenu() {
                   <img src={Binance} className="w-10 mr-5" alt="" />
                   Binance Chain Wallet
                 </li> */}
-                <li className="flex text-white font-bold text-lg items-center bg-secondary m-3 p-2 rounded-lg h-14 cursor-pointer" onClick={() => connectWallet("injected")}>
-                  <img src={Wallet} className="w-10 mr-5" alt="" />
-                  Wallet Connect
-                </li>
                 {/* <li className="flex text-white font-bold text-lg items-center bg-secondary m-3 p-2 rounded-lg h-14 cursor-pointer" onClick={() => connectWallet("injected")}>
                   <img src={Portis} className="w-10 mr-5" alt="" />
                   Portis
