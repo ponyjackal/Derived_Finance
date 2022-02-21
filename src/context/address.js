b6282edc5da8ebeb2d730568650b30dd93442a40
kecrypto <kelee.life@gmail.com>
2022-02-20 18:47:13 -0600

    feat: updated address

--- src/context/address.js
+++ src/context/address.js
@@ -5,42 +5,42 @@ export const contractAddresses = {
   },
   derivedToken: {
     56: "",
-    97: "0xE41B000268eDBFc239988237D7Cc6B995aD3e1Dc",
+    97: "0x92d1DAa0531c4eE709AA8a8aEe3C9A509769ff9e",
   },
   // Main ERC20
   dvdx: {
     56: "",
-    97: "0xE52Da10157ef818c35B5Ed3Ca9C76772816E2Eae",
+    97: "0x040a9e03917F66F62fb2050C0423fe39f741b974",
   },
   // Asset ERC20
   usdx: {
     56: "",
-    97: "0xE41B000268eDBFc239988237D7Cc6B995aD3e1Dc",
+    97: "0x92d1DAa0531c4eE709AA8a8aEe3C9A509769ff9e",
   },
   // Fee Pool
   feePool: {
     56: "",
-    97: "0x68DA04b1192831d59F71630cAEE47D8e516d07F5",
+    97: "0x34Da658bb0744943c69AE7acd5eb8dB332edcB91",
   },
   // Depot
   depot: {
     56: "",
-    97: "0x53643Df434Ec0C3C1294075Bb0a70efEe4dB71C7",
+    97: "0x25cBdE6D43Bd278d2668fF8c7eC9E9ab9f22bC3a",
   },
   exchangeRate: {
     56: "",
-    97: "0xCA4284bC135F9A96680497B24Be4E1e0e52643f2",
+    97: "0x7A24d87dFce20f954D16BF76FC1dD6916B9B3333",
   },
   btc: {
     56: "",
-    97: "0x1C5ad958898a401164717d511f59EFCE6817Cf37",
+    97: "0xDE16Fa6C8c6f4656D42bF2a410BB83Fd42842B24",
   },
   eth: {
     56: "",
-    97: "0x06Cd433E84D966E549D9a1De4Ea63C9062E2ae8D",
+    97: "0x2564c8718E548534537E3b95c54f924c411cE0B6",
   },
   bnb: {
     56: "",
-    97: "0x8fC450efC60aBe8AeaE982837e243Ec1E53778C6",
+    97: "0xd2cF2d544B2E2CA510863f24773d64ba946a6299",
   },
 };
