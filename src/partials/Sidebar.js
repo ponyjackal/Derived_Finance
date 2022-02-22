5f1b44341c6c1bf096b15048d3f190499fc04f2f
kecrypto <kelee.life@gmail.com>
2022-02-22 10:53:50 -0600

    feat: updated social links

--- src/partials/Sidebar.js
+++ src/partials/Sidebar.js
@@ -41,7 +41,7 @@ function SocialButton() {
     <React.Fragment>
       <Grid item xs={4}>
         <Item>
-          <a href="">
+          <a href="https://derived.fi/" target="_blank" rel="noreferrer">
             <LanguageOutlinedIcon />
           </a>
         </Item>
@@ -84,15 +84,21 @@ function SocialButton() {
       </Grid>
       <Grid item xs={4}>
         <Item className="flex justify-center">
-          <svg
-            fill="#86C440"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 50 50"
-            className="h-6 w-7"
+          <a
+            href="https://medium.com/@DerivedFinance"
+            target="_blank"
+            rel="noreferrer"
           >
-            {" "}
-            <path d="M 3 6 L 7 11.091797 L 7 35.285156 L 1 43 L 15 43 L 9 35.285156 L 9 13.75 L 22 43 L 21.998047 43.013672 L 34 13.544922 L 34 39 L 30 43 L 47 43 L 43 39 L 42.972656 10.503906 L 46.863281 6.0136719 L 34.845703 6.0136719 L 25.605469 28.744141 L 15.496094 6 L 3 6 z" />
-          </svg>
+            <svg
+              fill="#86C440"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 50 50"
+              className="h-6 w-7"
+            >
+              {" "}
+              <path d="M 3 6 L 7 11.091797 L 7 35.285156 L 1 43 L 15 43 L 9 35.285156 L 9 13.75 L 22 43 L 21.998047 43.013672 L 34 13.544922 L 34 39 L 30 43 L 47 43 L 43 39 L 42.972656 10.503906 L 46.863281 6.0136719 L 34.845703 6.0136719 L 25.605469 28.744141 L 15.496094 6 L 3 6 z" />
+            </svg>
+          </a>
         </Item>
       </Grid>
     </React.Fragment>
