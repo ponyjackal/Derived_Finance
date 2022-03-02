1541338c778b7c17fd01e17f0f79d16f76fce9dd
kecrypto <kelee.life@gmail.com>
2022-03-02 15:25:35 -0600

    feat: added balances on dashboard

--- src/context/finance/index.jsx
+++ src/context/finance/index.jsx
@@ -4,14 +4,20 @@ import { useWeb3React } from "@web3-react/core";
 
 import { useChain } from "../chain";
 import { stringToHex } from "../../utils/Contract";
+import { getPrice } from "../../services/coingecko";
 
 export const FinanceContext = createContext({});
 
 export const useFinance = () => useContext(FinanceContext);
 
 export const FinanceProvider = ({ children }) => {
-  const { USDXContract, DVDXContract, FeePoolContract, ExchangeRateContract, SynthContracts } =
-    useChain();
+  const {
+    USDXContract,
+    DVDXContract,
+    FeePoolContract,
+    ExchangeRateContract,
+    SynthContracts,
+  } = useChain();
   const { account } = useWeb3React();
 
   const [loadingBalances, setLoadingBalances] = useState(false);
@@ -22,6 +28,7 @@ export const FinanceProvider = ({ children }) => {
   const [transferableDVDX, setTransferableDVDX] = useState(new BigNumber(0));
   const [fees, setFees] = useState({});
   const [rates, setRates] = useState({});
+  const [dvdxPrice, setDVDXPrice] = useState(1);
 
   const fetchBalances = async () => {
     setLoadingBalances(true);
@@ -75,7 +82,14 @@ export const FinanceProvider = ({ children }) => {
 
     setSynthBalances(synthes);
 
+    await fetchDVDXPrice();
+
     setLoadingBalances(false);
+  };
+
+  const fetchDVDXPrice = async () => {
+    const price = await getPrice("derived");
+    setDVDXPrice(price);
   };
 
   useEffect(() => {
@@ -121,7 +135,9 @@ export const FinanceProvider = ({ children }) => {
         issuables,
         rates,
         transferableDVDX,
+        dvdxPrice,
         fetchBalances,
+        fetchDVDXPrice,
       }}
     >
       {children}
